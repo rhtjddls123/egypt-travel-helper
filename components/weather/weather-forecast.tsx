310e87ff8f0db2201c052e793f73c78844c8e134
rhtjddls123 <jjjk0605@naver.com>
2025-04-03 20:44:14 +0900

    fix: 데스크톱 화면에서 스크롤바때문에 라인 깨지는거 해결

--- components/weather/weather-forecast.tsx
+++ components/weather/weather-forecast.tsx
@@ -14,13 +14,13 @@ const WeatherForecast = async ({ cityName }: WeatherForecastProps) => {
   return (
     <div className="mt-4 pt-4 border-t">
       <div className="flex">
-        <div className="flex flex-col shrink-0 justify-end mr-1 text-xs text-gray-500">
+        <div className="flex flex-col shrink-0 justify-end mr-1 text-xs text-gray-500 pb-1">
           <span>강수 확률</span>
           <span>강수량</span>
           <span>습도</span>
           <span>바람</span>
         </div>
-        <div className="flex overflow-auto ">
+        <div className="flex horizontal-scroll">
           {data.map((info) => {
             const Icon = WEATHER_ICON[info.weather[0].icon as WeatherIconCode];
             const time = formatHour(info.dt_txt || "");
