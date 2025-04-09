7e82539f60c22333fc48a2ee78c16d2aeba9ea03
rhtjddls123 <jjjk0605@naver.com>
2025-04-09 23:39:19 +0900

    feat: 숫자 변환 기능 구현

--- components/tools/number-converter.tsx
+++ components/tools/number-converter.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import { ChangeEvent, useState } from "react";
 import { Input } from "../ui/input";
 import ToolsContentWrapper from "./tools-content-wrapper";
 
@@ -7,6 +9,32 @@ interface NumberConverterProps {
 }
 
 const NumberConverter = ({ id }: NumberConverterProps) => {
+  const [result, setReulst] = useState<string>();
+
+  const arabicNumerals: Record<string, string> = {
+    "0": "٠",
+    "1": "١",
+    "2": "٢",
+    "3": "٣",
+    "4": "٤",
+    "5": "٥",
+    "6": "٦",
+    "7": "٧",
+    "8": "٨",
+    "9": "٩"
+  };
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+
+    const converted = value
+      .split("")
+      .map((char) => arabicNumerals[char])
+      .join("");
+
+    setReulst(converted);
+  }
+
   return (
     <>
       <ToolsContentWrapper
@@ -17,12 +45,16 @@ const NumberConverter = ({ id }: NumberConverterProps) => {
           <div className="space-y-4">
             <div>
               <p className="mb-2 font-medium text-sm">일반 숫자 입력</p>
-              <Input className="h-10" placeholder="숫자를 입력하세요 (예: 123)" />
+              <Input
+                className="h-10"
+                onChange={handleInputChange}
+                placeholder="숫자를 입력하세요 (예: 123)"
+              />
             </div>
             <div>
               <p className="mb-2 font-medium text-sm">아랍 숫자 결과</p>
-              <div className="p-3 text-2xl text-center rounded-md border bg-slate-50">
-                결과가 여기에 표시됩니다
+              <div className="p-3 text-4xl text-center rounded-md border bg-slate-50">
+                {result ? result : <p className="text-2xl">결과가 여기에 표시됩니다</p>}
               </div>
             </div>
           </div>
@@ -32,7 +64,7 @@ const NumberConverter = ({ id }: NumberConverterProps) => {
           <ul className="ml-6 list-disc space-y-2">
             <li>이집트에서 아랍 숫자를 볼 때 참고하세요</li>
             <li>가격표, 시간표 등에서 자주 볼 수 있습니다</li>
-            <li>숫자만 변환되며 다른 문자는 그대로 유지됩니다</li>
+            <li>숫자만 변환되며 다른 문자는 삭제됩니다</li>
           </ul>
         }
       />
