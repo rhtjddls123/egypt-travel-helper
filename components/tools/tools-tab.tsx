6ac5eaa3c7333878ead9fc208bff2ec2d68dcd84
rhtjddls123 <jjjk0605@naver.com>
2025-04-19 22:19:34 +0900

    feat: 상형문자 변환기 toolsTab에 추가

--- components/tools/tools-tab.tsx
+++ components/tools/tools-tab.tsx
@@ -1,8 +1,12 @@
 import { Tabs } from "@/components/ui/tabs";
 import ToolsTabList from "./tools-tab-list";
 import NumberConverter from "./number-converter";
+import GlyphConverter from "./glyph-converter";
 
-const TOOLS_TAB_LIST = [{ id: "number", name: "숫자 변환", Component: NumberConverter }];
+const TOOLS_TAB_LIST = [
+  { id: "number", name: "숫자 변환", Component: NumberConverter },
+  { id: "glyph", name: "상형문자", Component: GlyphConverter }
+];
 
 const ToolsTab = () => {
   return (
