fe172019dc85ce778d8fc24420157ef0fcf8906c
rhtjddls123 <jjjk0605@naver.com>
2025-04-22 20:21:37 +0900

    feat: 아랍어 입력받아서 음성 buffer반환하는 api생성

---
+++ app/api/tts/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { getAudioUrl } from "google-tts-api";
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const text = searchParams.get("text");
+
+  if (!text) {
+    return NextResponse.json({ error: "No text provided" }, { status: 400 });
+  }
+
+  try {
+    const url = getAudioUrl(text, {
+      lang: "ar",
+      slow: false,
+      host: "https://translate.google.com"
+    });
+    const audioRes = await fetch(url);
+    const arrayBuffer = await audioRes.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    return new Response(buffer, {
+      headers: {
+        "Content-Type": "audio/mpeg",
+        "Content-Length": buffer.length.toString(),
+        "Cache-Control": "no-store"
+      }
+    });
+  } catch (error) {
+    console.log("여기?");
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
